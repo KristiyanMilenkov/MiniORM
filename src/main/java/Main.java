ab5fdf56ac3be427fd9546999bdacfb69c90ad30
Milenkov <krismessi1995@gmail.com>
2024-02-20 09:39:54 +0100

    Delete Method and some Refactoring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import Entities.User;
 import orm.EntityManager;
 
+import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.time.LocalDate;
@@ -9,12 +10,27 @@ import static orm.MyConnector.createConnection;
 import static orm.MyConnector.getConnection;
 
 public class Main {
-    public static void main(String[] args) throws SQLException, IllegalAccessException {
+    public static void main(String[] args) throws SQLException, IllegalAccessException, InvocationTargetException, InstantiationException, NoSuchMethodException {
         createConnection("root","*****","custom-orm");
         Connection connection = getConnection();
         EntityManager<User> userEntityManager = new EntityManager<>(connection);
         User user = new User("Boris",22, LocalDate.now());
-        userEntityManager.doCreate(User.class);
+        user.setUsername("Boris1.1");
+
+        //userEntityManager.doCreate(User.class);
+        //userEntityManager.doAlter(User.class);
         userEntityManager.persist(user);
+
+        Iterable<User> first = userEntityManager.find(User.class);
+        System.out.println(first.toString());
+
+        User toDelete = userEntityManager.findFirst(User.class, "id = 3");
+
+        userEntityManager.delete(toDelete);
+
+        Iterable<User> second = userEntityManager.find(User.class);
+        System.out.println(second.toString());
+
+        connection.close();
     }
 }
--- src/main/java/orm/DBContext.java
+++ src/main/java/orm/DBContext.java
@@ -1,15 +1,18 @@
 package orm;
 
+import java.lang.reflect.InvocationTargetException;
 import java.sql.SQLException;
 
 public interface DBContext<E> {
     boolean persist(E entity) throws IllegalAccessException, SQLException;
 
-    Iterable<E> find(Class<E> table) throws SQLException, InstantiationException, IllegalAccessException;
+    Iterable<E> find(Class<E> table) throws SQLException, InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException;
 
-    Iterable<E> find(Class<E> table, String where) throws SQLException, InstantiationException, IllegalAccessException;
+    Iterable<E> find(Class<E> table, String where) throws SQLException, InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException;
 
-    E findFirst(Class<E> table) throws SQLException, InstantiationException, IllegalAccessException;
+    E findFirst(Class<E> table) throws SQLException, InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException;
 
-    E findFirst(Class<E> table, String where) throws SQLException, InstantiationException, IllegalAccessException;
+    E findFirst(Class<E> table, String where) throws SQLException, InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException;
+
+    boolean delete(E toDelete) throws IllegalAccessException, SQLException;
 }
